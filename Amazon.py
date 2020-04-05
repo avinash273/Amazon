d91458507d172c305c4b13c9a43e3f0f99e48344
avinash273 <avinash.shanker@mavs.uta.edu>
2020-04-05 18:11:08 -0500

    3Sum

--- Amazon.py
+++ Amazon.py
@@ -221,3 +221,40 @@ class Solution:
                     matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         for l in matrix:
             l.reverse()
+
+--------------------------------------------------------------------------------
+3Sum
+--------------------------------------------------------------------------------
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        N, result = len(nums), []
+        for i in range(N):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            target = nums[i]*-1
+            s,e = i+1, N-1
+            while s<e:
+                if nums[s]+nums[e] == target:
+                    result.append([nums[i], nums[s], nums[e]])
+                    s = s+1
+                    while s<e and nums[s] == nums[s-1]:
+                        s = s+1
+                elif nums[s] + nums[e] < target:
+                    s = s+1
+                else:
+                    e = e-1
+        return result
+
+--------------------------------------------------------------------------------
+3Sum
+--------------------------------------------------------------------------------
