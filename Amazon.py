3383dbd5fb6e62d49e530c04306b074356dc0c54
avinash273 <avinash.shanker@mavs.uta.edu>
2020-04-05 18:21:02 -0500

    Group Anagrams

--- Amazon.py
+++ Amazon.py
@@ -256,5 +256,27 @@ class Solution:
         return result
 
 --------------------------------------------------------------------------------
-3Sum
+Group Anagrams
+--------------------------------------------------------------------------------
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return ans.values()
+
+# Time Complexity: O(NK \log K)O(NKlogK), where NN is the length of strs, and KK is the maximum length of a string in strs. The outer loop has complexity O(N)O(N) as we iterate through each string. Then, we sort each string in O(K \log K)O(KlogK) time.
+
+# Space Complexity: O(NK)O(NK), the total information content stored in ans.
+--------------------------------------------------------------------------------
+Group Anagrams
 --------------------------------------------------------------------------------
