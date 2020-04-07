db017b55501412e714e2570ae030a87e5ab43615
avinash273 <avinash.shanker@mavs.uta.edu>
2020-04-06 20:59:44 -0500

    Minimum Window Substring

--- Amazon.py
+++ Amazon.py
@@ -550,5 +550,37 @@ Space complexity : O(1)O(1).
 Because only a constant number of single-value variables are used, the space complexity is O(1)O(1).
 
 --------------------------------------------------------------------------------
- Roman to Integer
+Minimum Window Substring
+--------------------------------------------------------------------------------
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+
+Example:
+
+Input: S = "ADOBECODEBANC", T = "ABC"
+Output: "BANC"
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        need = collections.Counter(t)            #hash table to store char frequency
+        missing = len(t)                         #total number of chars we care
+        start, end = 0, 0
+        i = 0
+        for j, char in enumerate(s, 1):          #index j from 1
+            if need[char] > 0:
+                missing -= 1
+            need[char] -= 1
+            if missing == 0:                     #match all chars
+                while i < j and need[s[i]] < 0:  #remove chars to find the real start
+                    need[s[i]] += 1
+                    i += 1
+                need[s[i]] += 1                  #make sure the first appearing char satisfies need[char]>0
+                missing += 1                     #we missed this first char, so add missing by 1
+                if end == 0 or j-i < end-start:  #update window
+                    start, end = i, j
+                i += 1                           #update i to start+1 for next window
+        return s[start:end]
+
+--------------------------------------------------------------------------------
+Minimum Window Substring
 --------------------------------------------------------------------------------
