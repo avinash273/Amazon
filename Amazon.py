e5ea5a84515899215ec488d1a216abd328ad94dd
avinash273 <avinash.shanker@mavs.uta.edu>
2020-04-06 20:52:27 -0500

    Roman to Integer

--- Amazon.py
+++ Amazon.py
@@ -504,5 +504,51 @@ Time complexity : O(N)O(N) where NN represents the number of elements in the inp
 Space complexity : O(1)O(1) since don't use any additional array for our computations. The problem statement mentions that using the answeranswer array doesn't add to the space complexity.
 
 --------------------------------------------------------------------------------
-Product of Array Except Self
+ Roman to Integer
+--------------------------------------------------------------------------------
+Input: "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Input: "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+values = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+}
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        total = 0
+        i = 0
+        while i < len(s):
+            # If this is the subtractive case.
+            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
+                total += values[s[i + 1]] - values[s[i]]
+                i += 2
+            # Else this is NOT the subtractive case.
+            else:
+                total += values[s[i]]
+                i += 1
+        return total
+
+Time complexity : O(1)O(1).
+
+As there is a finite set of roman numerals, the maximum number possible number can be 3999, which in roman numerals is MMMCMXCIX. As such the time complexity is O(1)O(1).
+
+If roman numerals had an arbitrary number of symbols, then the time complexity would be proportional to the length of the input, i.e. O(n)O(n). This is assuming that looking up the value of each symbol is O(1)O(1).
+
+Space complexity : O(1)O(1).
+
+Because only a constant number of single-value variables are used, the space complexity is O(1)O(1).
+
+--------------------------------------------------------------------------------
+ Roman to Integer
 --------------------------------------------------------------------------------
