8786c787de504d926367fd78212b975c05029eaa
avinash273 <avinash.shanker@mavs.uta.edu>
2020-05-20 15:55:03 -0500

    Two sum

--- Leetcode.java
+++ Leetcode.java
@@ -1,5 +1,7 @@
 /*----------------------------------------------------------------------------*/
-// Reverse a character
+//Reverse a character
+//Time complexity O(n)
+//Space complexity O(1)
 class Solution {
     public void reverseString(char[] s) {
         int left = 0, right = s.length-1;
@@ -13,5 +15,21 @@ class Solution {
         }
     }
 }
-
 /*----------------------------------------------------------------------------*/
+//Two sum
+//Time complexity O(n)
+//Space complexity O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> checkSum = new HashMap<>();
+
+        for (int i=0; i<nums.length; i++){
+            int balance = target - nums[i];
+            if(checkSum.containsKey(balance)){
+                return new int[] {checkSum.get(balance),i};
+            }
+            checkSum.put(nums[i], i);
+        }
+        return null;
+    }
+}
