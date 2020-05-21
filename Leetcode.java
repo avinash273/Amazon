26d308cef647f7665be0c58d6f20f1aa335f8d72
avinash273 <avinash.shanker@mavs.uta.edu>
2020-05-20 20:25:16 -0500

    Longest non repeating substring

--- Leetcode.java
+++ Leetcode.java
@@ -19,6 +19,11 @@ class Solution {
 //Two sum
 //Time complexity O(n)
 //Space complexity O(n)
+/*
+Given nums = [2, 7, 11, 15], target = 9
+Because nums[0] + nums[1] = 2 + 7 = 9
+return [0, 1].
+*/
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> checkSum = new HashMap<>();
@@ -33,3 +38,37 @@ class Solution {
         return null;
     }
 }
+/*----------------------------------------------------------------------------
+Longest Substring
+//Time complexity O(n)
+//Space complexity O(n)
+Input: "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+*/
+public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 1)
+            return 1;
+        else if (s.length() == 0)
+            return 0;
+        char[] tokens = s.toCharArray();
+        HashMap<Character, Integer> storeChar = new HashMap<>();
+        int counter = 0, i = 0, j = 0, max_val = 0;
+        int strLen = tokens.length;
+        while (i < strLen) {
+            if (storeChar.containsKey(tokens[i])) {
+                i = storeChar.get(tokens[i]) + 1;
+                storeChar.clear();
+                counter = 0;
+            } else {
+                storeChar.put(tokens[i], i);
+                counter++;
+                max_val = Math.max(counter, max_val);
+                i++;
+            }
+
+        }
+        System.out.println(storeChar);
+        System.out.println(max_val);
+        return max_val;
+    }
